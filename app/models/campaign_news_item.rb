c5e72ce6eba6e8ef3bef93ed5bbe033766e379de
sdbradley <scott.bradley@daveramsey.com>
2017-12-23 16:02:14 -0600

    [O1-8] added scopes to campaign news endpoint

--- app/models/campaign_news_item.rb
+++ app/models/campaign_news_item.rb
@@ -2,11 +2,16 @@ class CampaignNewsItem < ApplicationRecord
   belongs_to :campaign
 
   scope :with_stakeholder_of, -> (stakeholder_id) { joins(:campaign).where(campaigns: {stakeholder__c: stakeholder_id}) }
+  scope :with_partner_of, -> (partner_id) { joins(:campaign).where(campaigns: {partner__c: partner_id}) }
+  scope :by_start_date, -> (date) { joins(:campaign).where("campaigns.start_date >= ?", date.to_i) }
+  scope :by_end_date, -> (date) { joins(:campaign).where("campaigns.end_date < ?", date.to_i) }
 
   scope :search, -> (fields) {
     query = self
     query = query.with_stakeholder_of(fields[:stakeholder__c]) if fields[:stakeholder__c].present?
-    query
+    query = query.with_partner_of(fields[:partner__c]) if fields[:partner__c].present?
+    query = query.by_start_date(fields[:start_date]) if fields[:start_date].present?
+    query = query.by_end_date(fields[:end_date]) if fields[:end_date].present?
   }
 
   def self.get(params)
