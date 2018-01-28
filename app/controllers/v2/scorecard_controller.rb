0e0d2864619e5f12db95cf67a6c6a3d3123b3b9e
sdbradley <scott.bradley@daveramsey.com>
2018-01-28 17:55:48 -0600

    [O1-45] added stats to scoreboard

---
+++ app/controllers/v2/scorecard_controller.rb
@@ -0,0 +1,16 @@
+class V2::ScorecardController < ApplicationController
+    def index
+        opportunities = OpportunityDetail.get(permitted_params)
+        response_body = {
+            opportunities: opportunities.map(&:to_h)
+        }
+        standard_response_for ServiceResponse.new(status: :success, status_code: 200, body: response_body.to_json)
+    end
+
+  private
+  
+  def permitted_params
+    params.permit(:start_date, :end_date, :partner__c)
+  end
+
+end
