4c0287c6196e55859396c91346de4382905a0370
sdbradley <scott.bradley@daveramsey.com>
2018-03-24 19:35:58 -0500

    added is_otp_client param

--- app/controllers/v2/accounts_controller.rb
+++ app/controllers/v2/accounts_controller.rb
@@ -1,6 +1,11 @@
 class V2::AccountsController < ApplicationController
     def index
-        accounts = Account.where(account_type: 'Prospect')
+        accounts = 
+          if only_otp_clients?
+            Account.where(is_otp_client: true).order(:name)
+          else
+            Account.where(account_type: 'Prospect').order(:name)
+          end
         response_body = {
             accounts: accounts
         }
@@ -13,4 +18,15 @@ class V2::AccountsController < ApplicationController
         }
         standard_response_for ServiceResponse.new(status: :success, status_code: 200, body: response_body.to_json)
     end
+
+    private
+
+    def only_otp_clients?
+      permitted_params[:otp_client] == "1"
+    end
+
+    def permitted_params
+      params.permit(:otp_client)
+    end
+
 end
