be51325be0c7588f4e5c7f979f6dadc44e88f588
sdbradley <scott.bradley@daveramsey.com>
2018-08-07 20:16:53 -0500

    [O1-161] fixed PDF encoding issue

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -3,6 +3,7 @@ PDFKit.configure do |config|
     config.wkhtmltopdf = '/opt/rubies/ruby-2.3.6/bin/wkhtmltopdf'
     config.default_options = {
         page_size: 'A4',
+        encoding: 'UTF-8',
         print_media_type: true,
         dpi: 400,
         footer_center: "Page [page] of [toPage]",
